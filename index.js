06030b3944f768fbb312455badf45f56bef9185f
azhararrozak <azhardrozak2001@upi.edu>
2024-02-14 12:51:12 +0700

    Change to https server

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const http = require("http");
+const https = require("https")
+const fs = require('fs');
 const socketIO = require("socket.io");
 
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer({
+  key: fs.readFileSync(process.env.PRIV_KEY),
+  cert: fs.readFileSync(process.env.FULL_CHAIN),
+}, app);
+
 const io = socketIO(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-    credentials: false
+    credentials: true
   }
 });
 
 const corsOptions = {
   origin: "*",
-  credentials: false,
+  credentials: true,
 };
 
 // middleware
@@ -68,9 +73,8 @@ app.get("/", (req, res) => {
 });
 
 // start server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+server.listen(443, () => {
+  console.log(`Server is running on port 443`);
 });
 
 // routes
