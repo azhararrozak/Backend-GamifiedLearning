8d125cf8a21c271dbec08a1fa33d6414bb3b2504
azhararrozak <azhardrozak2001@upi.edu>
2024-02-15 10:40:19 +0700

    fix quiz answer saved 3

--- src/controllers/quiz.controller.js
+++ src/controllers/quiz.controller.js
@@ -158,14 +158,28 @@ exports.submitPretest = async (req, res) => {
       }
     });
 
-    
-
     const scoreExist = await Score.findOne({ user: req.userId });
 
     //Jika score sudah ada tidak bisa submit lagi
     if (scoreExist) {
       res.status(400).send({ message: "Anda Sudah Mengerjakan Pretest ini" });
     } else {
+      questions.forEach(async (question, index) => {
+        const selectedOptionId = selectedAnswers[index]; // ID opsi yang dipilih oleh siswa
+        const isCorrect = question.options.some(option => option._id.toString() === selectedOptionId && option.isCorrect);
+      
+        const newAnswer = new AnswerPretest({
+          user: req.userId,
+          answer: [{
+            question: question._id,
+            selectedOption: selectedOptionId,
+            isCorrect,
+          }]
+        });
+      
+        await newAnswer.save();
+      });
+
       //Add point 5 berdasarkan jumlah benar dan hanya sekali submit
       const point = await Point.findOne({ user: req.userId });
 
@@ -223,29 +237,12 @@ exports.submitPostest = async (req, res) => {
     if (scoreExist) {
       res.status(400).send({ message: "Anda Sudah Mengerjakan Postest ini" });
     } else {
-      questions.forEach(async (question, index) => {
-        const selectedOptionId = selectedAnswers[index]; // ID opsi yang dipilih oleh siswa
-        const isCorrect = question.options.some(option => option._id.toString() === selectedOptionId && option.isCorrect);
-      
-        const newAnswer = new AnswerPretest({
-          user: req.userId,
-          answer: [{
-            question: question._id,
-            selectedOption: selectedOptionId,
-            isCorrect,
-          }]
-        });
-      
-        await newAnswer.save();
-      });
-      
       const newScore = new Score({
         posttest: score,
         user: req.userId,
       });
 
       await newScore.save();
-
       res.send({ score });
     }
   } catch (error) {
