d18148bdfd7dc8786882becb3941b62ca982ff51
azhararrozak <azhardrozak2001@upi.edu>
2024-04-27 11:07:49 +0700

    add unit controller

--- src/controllers/unit.controller.js
+++ src/controllers/unit.controller.js
@@ -3,7 +3,7 @@ const Unit = db.unit;
 
 exports.getUnits = async (req, res) => {
   try {
-    const units = await Unit.find();
+    const units = await Unit.find().sort({_id: 1});;
     res.send(units);
   } catch (err) {
     res.status(500).send({ message: err.message });
@@ -23,10 +23,31 @@ exports.createUnit = async (req, res) => {
   try {
     const unit = new Unit({
       title: req.body.title,
+      description: req.body.description,
+      lessons: [],
     });
 
     await unit.save();
-    res.send({ message: "Unit was created successfully!" });
+    res.send({ message: "Unit atau Pertemuan berhasil di buat" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+exports.updateUnit = async (req, res) => {
+  try {
+    await Unit.findByIdAndUpdate(req.params.id, req.body);
+    res.send({ message: "Unit atau Pertemuan berhasil di update" });
+  }
+  catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}
+
+exports.deleteUnit = async (req, res) => {
+  try {
+    await Unit.findByIdAndDelete(req.params.id);
+    res.send({ message: "Unit atau Pertemuan berhasil di hapus" });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
