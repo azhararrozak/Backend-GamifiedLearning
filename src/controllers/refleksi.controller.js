572c7143fa873f0a7210886d21c86958b5b1bb07
azhararrozak <azhardrozak2001@upi.edu>
2024-05-31 20:26:25 +0700

    fix controller refleksi

--- src/controllers/refleksi.controller.js
+++ src/controllers/refleksi.controller.js
@@ -3,7 +3,7 @@ const Refleksi = db.refleksi;
 
 exports.getRefleksi = async (req, res) => {
     try {
-        const refleksi = await Refleksi.find();
+        const refleksi = await Refleksi.find().populate("refleksi.userId", "-__v -password");
         res.status(200).json(refleksi);
     } catch (err) {
         res.status(500).json({ message: err.message });
